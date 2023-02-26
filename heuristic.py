4c26e4dbe735bb2b815065168f03e53d6c45ce48
Louis-Victor Ladagnous <louivictor@live.fr>
2023-02-26 01:36:38 +0100

    Factorisation de l'heuristique montecarlo à cause d'un bug perso

--- heuristic.py
+++ heuristic.py
@@ -2,23 +2,14 @@ import time
 import Goban 
 from random import choice
 
+
 def randomEndGame(board, color):
     if board.is_game_over():
         (black, white) = board.compute_score()
         if color == Goban.Board._BLACK:
-            if black > white:
-                return 1
-            elif white > black:
-                return -1
-            else:
-                return 0
+            return black - white
         else:
-            if black < white:
-                return 1
-            elif white < black:
-                return -1
-            else:
-                return 0
+            return white - black
     
     moves = board.legal_moves() 
     move = choice(moves) 
@@ -26,6 +17,7 @@ def randomEndGame(board, color):
     result = randomEndGame(board, color)
     board.pop()
     return result
+
 
 def MonteCarloHeuristic(board, color):
     heuristic = 0
